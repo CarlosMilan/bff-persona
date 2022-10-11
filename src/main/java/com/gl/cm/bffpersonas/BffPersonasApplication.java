7cca5afc6b5bb12dded2f3cc5b081736514b9080
CarlosMilan <martin.milan3@gmail.com>
2022-10-11 09:32:16 -0300

    Se crea proyecto en blanco

---
+++ src/main/java/com/gl/cm/bffpersonas/BffPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.gl.cm.bffpersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffPersonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gl/cm/bffpersonas/BffPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.cm.bffpersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
